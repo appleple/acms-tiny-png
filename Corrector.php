406009f83ea8530d2e027fece83ab8020c38b521
atsu666 <ito.atsu666@gmail.com>
2019-11-21 16:38:51 +0900

    add Hook

--- Corrector.php
+++ Corrector.php
@@ -13,46 +13,51 @@ class Corrector
      */
     public function resizeTinyPng($src, $args)
     {
-        $type = isset($args[0]) ? $args[0] : 'scale'; // scale | fit | cover | thumb
-        $width = isset($args[1]) ? intval($args[1]) : 0;
-        $height = isset($args[2]) ? intval($args[2]) : 0;
-        $pfx = 'mode_tinypng_';
-        if (!empty($width)) {
-            $pfx .= 'w' . $width;
-        }
-        if (!empty($height)) {
-            if (!empty($pfx)) {
-                $pfx .= '_';
+        try {
+            $type = isset($args[0]) ? $args[0] : 'scale'; // scale | fit | cover | thumb
+            $width = isset($args[1]) ? intval($args[1]) : 0;
+            $height = isset($args[2]) ? intval($args[2]) : 0;
+            $pfx = 'mode_tinypng_';
+            if (!empty($width)) {
+                $pfx .= 'w' . $width;
             }
-            $pfx .= 'h' . $height;
-        }
-        foreach (array('', REQUEST_URL, ARCHIVES_DIR, REVISON_ARCHIVES_DIR, MEDIA_LIBRARY_DIR) as $archive_dir) {
-            $tmpPath = $archive_dir . normalSizeImagePath($src);
-            $destPath = trim(dirname($tmpPath), '/') . '/' . $pfx . '-' . Storage::mbBasename($tmpPath);
-            $destPathVars = trim(dirname($src), '/') . '/' . $pfx . '-' . Storage::mbBasename($tmpPath);
-            $largePath = otherSizeImagePath($tmpPath, 'large'); // large path
+            if (!empty($height)) {
+                if (!empty($pfx)) {
+                    $pfx .= '_';
+                }
+                $pfx .= 'h' . $height;
+            }
+            foreach (array('', REQUEST_URL, ARCHIVES_DIR, REVISON_ARCHIVES_DIR, MEDIA_LIBRARY_DIR) as $archive_dir) {
+                $tmpPath = $archive_dir . normalSizeImagePath($src);
+                $destPath = trim(dirname($tmpPath), '/') . '/' . $pfx . '-' . Storage::mbBasename($tmpPath);
+                $destPathVars = trim(dirname($src), '/') . '/' . $pfx . '-' . Storage::mbBasename($tmpPath);
+                $largePath = otherSizeImagePath($tmpPath, 'large'); // large path
 
-            if (Storage::isReadable($destPath)) {
-                return $destPathVars;
+                if (Storage::isReadable($destPath)) {
+                    return $destPathVars;
+                }
+                if (Storage::isReadable($largePath)) {
+                    $srcPath = $largePath;
+                    break;
+                }
+                if (Storage::isReadable($tmpPath)) {
+                    $srcPath = $tmpPath;
+                    break;
+                }
             }
-            if (Storage::isReadable($largePath)) {
-                $srcPath = $largePath;
-                break;
+            if (empty($srcPath)) {
+                return $src;
             }
-            if (Storage::isReadable($tmpPath)) {
-                $srcPath = $tmpPath;
-                break;
+            if (!$xy = Storage::getImageSize($srcPath)) {
+                return $src;
             }
-        }
-        if (empty($srcPath)) {
-            return $src;
-        }
-        if (!$xy = Storage::getImageSize($srcPath)) {
-            return $src;
-        }
-        $engine = \App::make('tiny_png');
-        $engine->resize($srcPath, $destPath, $type, $width, $height);
+            $engine = \App::make('tiny_png');
+            $engine->resize($srcPath, $destPath, $type, $width, $height);
 
-        return $destPathVars;
+            return $destPathVars;
+        } catch (\Exception $e) {
+            userErrorLog($e->getMessage());
+        }
+        return $src;
     }
 }
--- Hook.php
+++ Hook.php
@@ -2,8 +2,12 @@
 
 namespace Acms\Plugins\TinyPNG;
 
+use Acms\Services\Facades\Storage;
+
 class Hook
 {
+    protected $tinyPng = false;
+
     /**
      * メディアデータ作成
      * @param string $path 作成先パス
@@ -11,6 +15,16 @@ class Hook
      */
     public function mediaCreate($path)
     {
-
+        try {
+            if (!Storage::getImageSize($path)) {
+                return;
+            }
+            if ($this->tinyPng === false) {
+                $this->tinyPng = \App::make('tiny_png');
+            }
+            $this->tinyPng->compress($path, $path);
+        } catch (\Exception $e) {
+            userErrorLog($e->getMessage());
+        }
     }
 }
